41e60ca024c7f95f781044cc59e3239bc309e4c1
jairoegc <jairoegc@gmail.com>
2020-05-10 19:33:48 -0400

    New counter added at processing_unit, not tested yet

--- proyecto_pipe/proyecto_pipe.srcs/sources_1/new/processing_unit.sv
+++ proyecto_pipe/proyecto_pipe.srcs/sources_1/new/processing_unit.sv
@@ -47,26 +47,34 @@ module processing_unit #(parameter NBYTES = 1024)(
     output done
     );
     
-    localparam NBYTES2 = 2;
+    localparam NBYTES2 = 3;
     
     localparam READA = 2'b00;
     localparam READB = 2'b01;
     localparam SUMVEC = 2'b10;
     localparam AVGVEC = 2'b11; 
+
+    localparam COUNTER_MAX = $clog2(NBYTES);
+    localparam COUNTER_SIZE = $clog2(COUNTER_MAX);
     
     enum logic {IDLE, PROCESS} state, next_state;
     logic store, ready;
     logic [7:0] vectorC [(NBYTES - 1):0];
     logic [7:0] sumVector [(NBYTES - 1):0];
     logic [7:0] avgVector [(NBYTES - 1):0];
+    logic [7:0] difVector [(NBYTES - 1):0];
     
-    assign sumVector = '{default:8'b0};
-    assign avgVector = '{default:8'b0}; 
+    logic [COUNTER_SIZE-1:0] counter = 'd0;
+    logic counter_rst;
+
+    //assign sumVector = '{default:8'b0};
+    //assign avgVector = '{default:8'b0}; 
     
     assign set = store;
     assign result = vectorC;
     assign done = ready;
     
+    //FSM FF
     always_ff @ (posedge clk) begin
         if (reset == 1'b1) begin
             state <= IDLE;
@@ -75,12 +83,48 @@ module processing_unit #(parameter NBYTES = 1024)(
             state <= next_state;
         end
     end
+
+    //Counter FF
+    logic
+    always_ff @ (posedge clk) begin
+        if ((reset == 1'b1)||(counter_rst)) begin
+            counter <= 1'd0;
+        end
+        else begin
+            counter <= counter + 1'd1;
+        end
+    end
+
+    genvar k;
+    generate
+        for ( k = 0; k < NBYTES; k = k + 1) begin: sum_loop
+            assign sumVector[k] = vectorA[k] + vectorB[k];
+            assign avgVector[k] = sumVector[k] >> 1;
+            assign difVector[k] = (vectorA[k] > vectorB[k]) ? vectorA[k] - vectorB[k] : vectorB[k] - vectorA[k];
+        end
+    endgenerate
+    
+    logic [(7 + $clog2(NBYTES)):0] manDist;
+    logic [(7 + $clog2(NBYTES)):0] manDist2;
+    
+    pipe_adder_tree #(.NBYTES(NBYTES)) pipe_adder (
+        .clk(clk),
+        .reset(reset),
+        .inVector(difVector),
+        .out(manDist2)
+        );
+        
+    combo_adder_tree #(.NBYTES(NBYTES)) combo_adder (
+        .inVector(difVector),
+        .out(manDist)
+        );
     
     always_comb begin
         next_state = state;
         store = 1'b0;
         ready = 1'b0;
         vectorC = '{default:8'b0};
+        counter_rst = 1'd1;
         case (state) 
             IDLE: begin
                 if (start == 1'b1)
@@ -89,6 +133,7 @@ module processing_unit #(parameter NBYTES = 1024)(
                     ready = 1'b1;
             end
             PROCESS: begin
+                counter_rst = 1'd0;
                 if(control[2] == 1) begin // vector type result operations
                     case(control[1:0])
                         READA:
@@ -100,14 +145,23 @@ module processing_unit #(parameter NBYTES = 1024)(
                         AVGVEC:
                             vectorC = avgVector;
                     endcase
+                    next_state = IDLE; 
+                    store = 1'b1;
+                    ready = 1'b1;
                 end
                 else begin // Manhattan distance
-                    vectorC[(NBYTES2 - 1):0] = '{default:8'b1};
-                    vectorC[(NBYTES - 1):NBYTES2] = '{default:8'b0};
+                    if (counter == (COUNTER_MAX-1)) begin
+                        vectorC[0] = manDist[7:0];
+                        vectorC[(NBYTES2 - 1):1] = '{default:8'b1}; //manDist[(NBYTES2 - 1):0];
+                        vectorC[(NBYTES - 1):NBYTES2] = '{default:8'b0};
+                        next_state = IDLE; 
+                        store = 1'b1;
+                        ready = 1'b1;
+                    end
                 end 
-                next_state = IDLE; 
-                store = 1'b1;
-                ready = 1'b1;
+                // next_state = IDLE; 
+                // store = 1'b1;
+                // ready = 1'b1;
             end
         endcase
     end
